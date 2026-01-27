fddc89408bbc0da887b4a70560ee134a647a01aa
bechovang <phuchcm2006@gmail.com>
2026-01-28 03:22:32 +0700

    update auto build

--- latex/run.cmd
+++ latex/run.cmd
@@ -1,14 +1,21 @@
 @echo off
 color 0A
+title Tu dong bien dich LaTeX (Bo qua loi)
 
-xelatex main.tex
-xelatex main.tex
+echo Dang chay Xelatex lan 1...
+:: Them -interaction=nonstopmode de khong dung lai khi gap loi
+xelatex -interaction=nonstopmode -synctex=1 main.tex
 
 echo.
-echo ==============================
-echo   Da bien dich xong!
-echo   Nhan Enter de thoat
-echo ==============================
+echo Dang chay Xelatex lan 2 (de cap nhat Muc luc va Tham chieu)...
+:: Chay lan 2 de dam bao so trang va muc luc dung
+xelatex -interaction=nonstopmode -synctex=1 main.tex
+
+echo.
+echo ============================================
+echo   DA BIEN DICH XONG (DA TU DONG SKIP LOI)!
+echo   Kiem tra file main.pdf nhe.
+echo ============================================
 echo.
 
 pause > nul
